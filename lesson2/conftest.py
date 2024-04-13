b57c3b7a9b6ee282b9cdc8238b8ec2191b159087
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-04-13 11:04:12 +0500

    conftest updated

--- lesson2/conftest.py
+++ lesson2/conftest.py
@@ -4,11 +4,17 @@ from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
-def browser():
-    # chrome_options = Options()
-    # chrome_options.add_argument('--headless')
-    # browser = webdriver.Chrome(options=chrome_options)
-    browser = webdriver.Chrome()
+def chrome_options():
+    options = Options()
+    options.add_argument('--headless')
+    return options
+
+
+@pytest.fixture()
+def browser(chrome_options):
+    browser = webdriver.Chrome(options=chrome_options)
+    # browser = webdriver.Chrome()
+    browser.implicitly_wait(10)
     yield browser
     browser.quit()
 
